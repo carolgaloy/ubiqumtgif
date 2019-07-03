a4e0cfbff78b2719da0d92ab6069fe829d44a952
Carolina Garcia-Loygorri Domenech <carolina.garcia-loygorri@in2.es>
2019-07-03 20:14:38 +0200

    Second Table Creation

---
+++ scripts/dataload-senate.js
@@ -0,0 +1,37 @@
+let members = data.results[0].members;
+
+function createTable (senateData) {
+
+    let housedata = document.getElementById("senate-data");
+
+    for (let i = 0; i < senateData.length; i++) {
+        
+        let tr = document.createElement("tr");
+
+        let name = document.createElement("td");
+        name = senateData[i].first_name;
+        if (senateData[i].middle_name != null) {
+            name = name + " " + senateData[i].middle_name;
+        }
+        name = name + " " + senateData[i].last_name;
+        name.textContent = name;
+
+        let party = document.createElement("td");
+        party.textContent = senateData[i].party;
+
+        let state = document.createElement("td");
+        state.textContent = senateData[i].state;
+
+        let yearsInOffice = document.createElement("td");
+        yearsInOffice.textContent = senateData[i].seniority;
+
+        let votes = document.createElement("td");
+        votes.textContent = senateData[i].votes_with_party_pct;
+
+        tr.append(name, party, state, yearsInOffice, votes);
+        housedata.appendChild(tr);
+        
+    }
+}
+
+createTable(members);
