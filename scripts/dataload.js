a2185c2022092a0204f6d08b47d3e67f15790b24
Carolina Garcia-Loygorri Domenech <carolina.garcia-loygorri@in2.es>
2019-07-03 20:08:22 +0200

    Table Creation

--- scripts/dataload.js
+++ scripts/dataload.js
@@ -1,1 +1,37 @@
-document.getElementById("senate-data").innerHTML = JSON.stringify(data,null,2);
+let members = data.results[0].members;
+
+function createTable (houseData) {
+
+    let housedata = document.getElementById("house-data");
+
+    for (let i = 0; i < houseData.length; i++) {
+        
+        let tr = document.createElement("tr");
+
+        let name = document.createElement("td");
+        name = houseData[i].first_name;
+        if (houseData[i].middle_name != null) {
+            name = name + " " + houseData[i].middle_name;
+        }
+        name = name + " " + houseData[i].last_name;
+        name.textContent = name;
+
+        let party = document.createElement("td");
+        party.textContent = houseData[i].party;
+
+        let state = document.createElement("td");
+        state.textContent = houseData[i].state;
+
+        let yearsInOffice = document.createElement("td");
+        yearsInOffice.textContent = houseData[i].seniority;
+
+        let votes = document.createElement("td");
+        votes.textContent = houseData[i].votes_with_party_pct;
+
+        tr.append(name, party, state, yearsInOffice, votes);
+        housedata.appendChild(tr);
+        
+    }
+}
+
+createTable(members);
